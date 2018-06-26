fa75556cd55a376424d026c6dd54d2c05b0e58f2
Unknown <edu.r994@gmail.com>
2018-06-25 23:06:13 -0300

    modificaciones de base de datos

--- database/migrations/2018_06_26_013516_create_personasprofesiones_table.php
+++ database/migrations/2018_06_26_013516_create_personasprofesiones_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePersonasprofesionesTable extends Migration
+class CreatePersonasProfesionesTable extends Migration
 {
     /**
      * Run the migrations.
