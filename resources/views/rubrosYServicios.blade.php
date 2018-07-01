d699409a7a1ebea1cccba246c24b27641804ca8f
Unknown <edu.r994@gmail.com>
2018-07-01 12:58:54 -0300

    se modifica la lista de servicios

--- resources/views/rubrosYServicios.blade.php
+++ resources/views/rubrosYServicios.blade.php
@@ -9,21 +9,30 @@
 	<div class="row">
 		<div class="col-md-12">
 		    <ol class="tree-structure">
+
 			    @foreach ($rubros as $rubro)
-	              <li>
-	                 <span class="num">{{ $rubro->id_rubro }}</span>
-	                 <a href="#">{{ $rubro->nombre_rubro }} </a>
-	                 <ol>
-	                 	@foreach ($servicios as $servicio)
-		                    <li>
-		                       <span class="num">{{ $rubro->id_rubro }}.{{ $servicio->id_servicio }}</span>
-		                       <a href="#">{{ $servicio->nombre_servicio }}</a>
-		                    </li>
-	                    @endforeach
-	                 </ol>
-	              </li>
-	            @endforeach
-           </ol>
+			    	<div class="col-md-4 article-img" >
+			    		<ul class="list-group">
+			    			
+							<h3><a class="num" href="#">{{ $rubro->nombre_rubro }} </a></h3>
+							
+								<ol>
+									@foreach ($servicios as $servicio)
+
+										@if($rubro->id_rubro === $servicio->id_rubro)
+											<li>
+												<span class="num">*</span>
+												<h4><a href="#" class="text-left">{{ $servicio->nombre_servicio }}</a></h4>
+											</li>
+										@endif
+
+									@endforeach
+								</ol>
+						</ul>
+					</div>
+				@endforeach
+
+			</ol>
 		</div>
 	</div>
 </div>
