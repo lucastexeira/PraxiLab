aa6ccb119230ec523a7dc48f3468477073f976f8
Unknown <edu.r994@gmail.com>
2018-07-01 14:09:33 -0300

    Cambios boludos

--- resources/views/rubrosYServicios.blade.php
+++ resources/views/rubrosYServicios.blade.php
@@ -9,17 +9,17 @@
 	<div class="row">
 		<div class="col-md-12">
 		    <ol class="tree-structure">
-
+		    	<h1 class="typo">Rubros</h1>
 			    @foreach ($rubros as $rubro)
-			    	<div class="col-md-3 article-img" >
+			    	<div class="col-md-3 article-img">
 			    		<ul class="list-group" id="list-tab" role="tablist">
 
 							<!--<a href="#" ><img width="75" height="55" src="{{ $rubro->imagen }}" alt="" /><h3>{{ $rubro->nombre_rubro }}</h3></a>-->
-
-							<a data-toggle="modal" class="b-link-fade b-animate-go" href="#myModal"  class="center-block"><img class="center-block" width="75" height="55" src="{{ $rubro->imagen }}" alt="" />
+							<div  class="center-block">
+							<a class="b-link-fade b-animate-go"  class="mask flex-center rgba-blue-light" href="#"><img class="center-block" width="75" height="55" src="{{ $rubro->imagen }}" />
 								<h4 class= "text-center">{{ $rubro->nombre_rubro }}</h4>
 							</a>
-							
+							</div>
 								<ol>
 									@foreach ($servicios as $servicio)
 
