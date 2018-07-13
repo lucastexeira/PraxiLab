dffcf8be2faae129fecdb02cdb54c0663026de86
Unknown <edu.r994@gmail.com>
2018-07-13 15:53:23 -0300

    Agregos servicios a la db

--- resources/views/rubrosYServicios.blade.php
+++ resources/views/rubrosYServicios.blade.php
@@ -17,9 +17,11 @@
 			    </div>
 			    @foreach ($rubros as $rubro)
 			    	<div class="col-md-3 article-img">
-			    		<br>
+			    		
 			    		<ul class="list-group" id="list-tab" role="tablist">
 							<div  class="center-block">
+							<br>
+							<br>
 							<a class="b-link-fade b-animate-go"  class="mask flex-center rgba-blue-light" href="servicios/{{ $rubro->id }}"><img class="center-block" width="75" height="55" src="{{ $rubro->imagen }}" />
 								<h2 class= "text-center">{{ $rubro->nombre_rubro }}</h2>
 							</a>
@@ -30,7 +32,6 @@
 									@endif
 
 								@endforeach
-
 							</div>
 						</ul>
 					</div>
