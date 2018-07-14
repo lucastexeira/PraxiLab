096b3386ae6cd48df8aae542da68fd366ffc079e
Unknown <karenjesica@gmail.com>
2018-07-14 12:15:34 -0300

    se agrego modal y cambios en wizard

--- resources/views/oferta.blade.php
+++ resources/views/oferta.blade.php
@@ -28,7 +28,27 @@
 								<p class="cantidad-opiniones-oferta">2 opiniones</p>
 							</div>
 							<div class="div-boton-oferta">
-								<button type="button" class="btn btn-lg btn-purple btn-oferta">Practicar</button>
+								<button type="button" class="btn btn-lg btn-purple btn-oferta" data-toggle="modal" data-target="#myModal">Practicar</button>
+							</div>
+
+							<!-- Modal -->
+							<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+							  <div class="modal-dialog" role="document">
+							    <div class="modal-content">
+							      <div class="modal-header">
+							        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+							      </div>
+							      <div class="modal-body">
+							        ¿Está seguro que desea iniciar la práctica?
+							      </div>
+							      <div class="modal-footer">
+							        <button type="button" class="btn btn-default" data-dismiss="modal">Volver</button>
+							        <a href="{{ 'listadoPracticasEstados' }}">
+							        	<button type="button" class="btn btn-primary">Continuar</button>
+							        </a>
+							      </div>
+							    </div>
+							  </div>
 							</div>
 
 						</div>
