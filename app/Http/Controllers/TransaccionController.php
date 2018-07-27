c6f023ee3c550d39f81e327e3d6757d6a07f0828
Unknown <edu.r994@gmail.com>
2018-07-27 20:09:07 -0300

    join con tabla personas

--- app/Http/Controllers/TransaccionController.php
+++ app/Http/Controllers/TransaccionController.php
@@ -36,11 +36,13 @@ class TransaccionController extends Controller
         $personaTransaccion = DB::table('transacciones')
                             ->leftjoin('historial_practicas', 'historial_practicas.id','=','transacciones.historial_practica')
                             ->leftjoin('practicas', 'practicas.id','=','historial_practicas.id_practica')
-                            ->select('transacciones.created_at')
+                            ->join('personas', 'personas.id','=','transacciones.id_emisor')
                             ->where('id_emisor',$usuario)
                             ->orWhere('id_destinatario', $usuario)
                             ->orderBy('transacciones.created_at','desc')
                             ->get();
+
+        
         
         //return view('/transacciones')->with('rubros',$rubros);
 
