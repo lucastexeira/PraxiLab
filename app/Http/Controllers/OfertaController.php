18f13849951f6618b65c6f71f9090e6f448a2045
Unknown <edu.r994@gmail.com>
2018-07-28 18:04:04 -0300

    Cambio el nombre de la variable que recibe por url
    Esto es porque se pisaba con el id para mostrar el usuario en la barra

--- app/Http/Controllers/OfertaController.php
+++ app/Http/Controllers/OfertaController.php
@@ -25,7 +25,7 @@ use DateTime;
 
 class OfertaController extends Controller
 {
-    public function oferta($id){ 
+    public function oferta($id_oferta){ 
 
         $req = Session::get('mail');
         $id = Persona::where('mail', $req)->first()->id;
@@ -35,7 +35,7 @@ class OfertaController extends Controller
         $rubros = Rubro::all();
         $practicaPersona = DB::Select('select practicas.id as practica_id, nombre_practica, img, imagen_practica, username, precio, practicas.descripcion from practicas 
                                        inner join personas on practicas.id_practicante = personas.id 
-                                       where practicas.id = '.$id.'');
+                                       where practicas.id = '.$id_oferta.'');
 
         /*$practicaPersona= Practica::where('id', $id)->first();
         $practicaPersona->Persona = Persona::where('id', $practicaPersona->id_practicante)->first();*/
